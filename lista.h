00b57b0ba4585db517f644be257c2b41ef5cecff
Mateus Gontijo <>
2021-04-14 23:02:02 +0000

    Inclusão da biblioteca

---
+++ lista.h
@@ -0,0 +1,63 @@
+#ifdef LISTA_H
+#define LISTA_H
+
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+// DEFINICAO DO TIPO PRODUTO
+typedef struct Produto{
+  int codigo;
+  char descricao[100];
+  float valor;
+  int quantidade;
+}Produto;
+
+// DEFINICAO DO TIPO PRODUTO
+typedef struct Produto{
+  int codigo;
+  char descricao[100];
+  float valor;
+  int quantidade;
+}Produto;
+
+// DEFINICAO DO TIPO CELULA
+typedef struct Celula{
+  Produto dado;
+  struct Celula *prox;
+}Celula;
+
+// DEFINICAO DO TIPO CELULA
+typedef struct Celula{
+  Produto dado;
+  struct Celula *prox;
+}Celula;
+
+
+// DEFINICAO DO TIPO LISTA
+typedef struct Lista{
+  Celula *inicio;
+  Celula *fim;
+  int tam;
+}Lista;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
